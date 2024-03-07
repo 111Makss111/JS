a040299ce65e1c3d80619c1e9d5de8fed3c10e1f
111Makss111 <151084313+111Makss111@users.noreply.github.com>
2024-03-07 16:12:19 +0100

    .

---
+++ JS/menu.js
@@ -0,0 +1,14 @@
+`use strict`;
+const number = 23 % 2;
+console.log(number);
+let result = "Some phrase";
+result = 10;
+console.log(result);
+const b = 9;
+const sum = b + 1;
+function fon() {
+  const question = prompt("Enter your Emeil");
+  console.log(question);
+  return question;
+}
+fon();
