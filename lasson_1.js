90941e6ed53b3f3a8feddf4d6b8729f49a8c8193
111Makss111 <777docmax777@gmail.com>
2024-09-05 21:01:29 +0200

    s

---
+++ lasson_1.js
@@ -0,0 +1,12 @@
+console.log("Hello Maks");
+const a = NaN;
+console.log(typeof a);
+const b = 4;
+const c = 2;
+console.log(b ** c);
+let students = "student";
+students -= 38;
+console.log(students);
+let str = "Hello"
+
+console.log(str[0]);
