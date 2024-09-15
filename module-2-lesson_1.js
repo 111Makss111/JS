c04b7a65f7b41cb8c2c8998d9f7a3650d87e6a0b
111Makss111 <777docmax777@gmail.com>
2024-09-15 12:42:37 +0200

    ю

--- module-2-lesson_1.js
+++ module-2-lesson_1.js
@@ -3,8 +3,10 @@
 const points = 500;
 
 if(points < 500){
-    console.log(level 1);    
+    console.log("level 1");    
 }  else if(points < 500){
     console.log(  );
-    
 }
+console.log(false && 5 && null);
+console.log(5 && null);
+
