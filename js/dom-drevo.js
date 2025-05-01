6a0dedf2696060323751fa8d5a30d09d808a2f71
111Makss111 <777docmax777@gmail.com>
2025-05-01 20:29:52 +0200

    js3

---
+++ js/dom-drevo.js
@@ -0,0 +1,37 @@
+// 1. Отримати текст із HTML-елемента за ID
+// const textElem = document.getElementById("text");
+// console.log(textElem.textContent);
+
+// 2. Змінити текст кнопки після натискання
+
+// const buttonElem = document.querySelector(".btn");
+// buttonElem.addEventListener("click", function () {
+//   buttonElem.innerText = "sell";
+// });
+
+// 3. Показати/сховати елемент
+
+// const buttonElem = document.querySelector(".btn");
+// const textElem = document.querySelector(".text");
+// buttonElem.addEventListener("click", function () {
+//   if (textElem.style.display === "none") {
+//     buttonElem.innerText = "sell";
+//     textElem.style.display = "block";
+//   } else {
+//     buttonElem.innerText = "bay";
+//     textElem.style.display = "none";
+//   }
+// });
+// 4. Змінити колір елемента
+// const buttonElem = document.querySelector(".btn");
+// const textElem = document.querySelector(".text");
+// buttonElem.addEventListener("click", function () {
+//   textElem.style.color = "Blue";
+//   if (textElem.style.display === "none") {
+//     buttonElem.innerText = "sell";
+//     textElem.style.display = "block";
+//   } else {
+//     buttonElem.innerText = "bay";
+//     textElem.style.display = "none";
+//   }
+// });
